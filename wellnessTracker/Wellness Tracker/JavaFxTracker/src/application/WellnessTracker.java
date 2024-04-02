ff9c5dd5b6360b5149ba9942068ffa9b71439b15
AnthonyAl-khafaji <127707907+AnthonyAl-khafaji@users.noreply.github.com>
2024-04-02 15:48:28 -0400

    Update WellnessTracker.java

--- wellnessTracker/Wellness Tracker/JavaFxTracker/src/application/WellnessTracker.java
+++ wellnessTracker/Wellness Tracker/JavaFxTracker/src/application/WellnessTracker.java
@@ -8,27 +8,12 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.util.Date;
+
 
 public class WellnessTracker extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // Creating labels and text fields for user input
-        Label exerciseNameLabel = new Label("Exercise Name:");
-        TextField exerciseNameField = new TextField();
-
-        Label exerciseDurationLabel = new Label("Exercise Duration (mins):");
-        TextField exerciseDurationField = new TextField();
-
-        Label exerciseCaloriesLabel = new Label("Calories Burned:");
-        TextField exerciseCaloriesField = new TextField();
-
-        Label workoutDurationLabel = new Label("Workout Duration (mins):");
-        TextField workoutDurationField = new TextField();
-
-        Label workoutCaloriesLabel = new Label("Calories Burned:");
-        TextField workoutCaloriesField = new TextField();
 
         Label usernameLabel = new Label("Username:");
         TextField usernameField = new TextField();
@@ -36,10 +21,17 @@ public class WellnessTracker extends Application {
         Label emailLabel = new Label("Email:");
         TextField emailField = new TextField();
 
-        Label passwordLabel = new Label("Password:");
-        TextField passwordField = new TextField();
+        // Creating labels and text fields for user input
+        Label exerciseNameLabel = new Label("Exercise Type (Weightlifting, Cardio, etc.):");
+        TextField exerciseNameField = new TextField();
 
-        Label reminderMessageLabel = new Label("Reminder Message:");
+        Label workoutDurationLabel = new Label("Workout Duration (mins):");
+        TextField workoutDurationField = new TextField();
+
+        Label workoutCaloriesLabel = new Label("Calories Burned(kcal):");
+        TextField workoutCaloriesField = new TextField();
+
+        Label reminderMessageLabel = new Label("Workout Note:");
         TextField reminderMessageField = new TextField();
 
         Button submitButton = new Button("Submit");
@@ -48,27 +40,31 @@ public class WellnessTracker extends Application {
         VBox root = new VBox(10);
         root.getChildren().addAll(
                 exerciseNameLabel, exerciseNameField,
-                exerciseDurationLabel, exerciseDurationField,
-                exerciseCaloriesLabel, exerciseCaloriesField,
                 workoutDurationLabel, workoutDurationField,
                 workoutCaloriesLabel, workoutCaloriesField,
                 usernameLabel, usernameField,
                 emailLabel, emailField,
-                passwordLabel, passwordField,
                 reminderMessageLabel, reminderMessageField,
                 submitButton
         );
 
         // Setting action for the submit button
         submitButton.setOnAction(event -> {
-            // Get user input and create objects
-            Exercise exercise = new Exercise(1, exerciseNameField.getText(), Integer.parseInt(exerciseDurationField.getText()), Integer.parseInt(exerciseCaloriesField.getText()));
-            Workout workout = new Workout(1, new Date(), Integer.parseInt(workoutDurationField.getText()), Integer.parseInt(workoutCaloriesField.getText()));
-            User user = new User(1, usernameField.getText(), emailField.getText(), passwordField.getText());
-            Reminder reminder = new Reminder(1, new Date(), reminderMessageField.getText());
+            // Get user input
+            String exerciseName = exerciseNameField.getText();
+            int workoutDuration = Integer.parseInt(workoutDurationField.getText());
+            int workoutCalories = Integer.parseInt(workoutCaloriesField.getText());
+            String username = usernameField.getText();
+            String email = emailField.getText();
+            String reminderMessage = reminderMessageField.getText();
 
-            // Displaying input data
-            displayData(exercise, workout, user, reminder);
+            // Display input data in the console
+            System.out.println("Exercise Name: " + exerciseName);
+            System.out.println("Workout Duration: " + workoutDuration + " mins");
+            System.out.println("Workout Calories: " + workoutCalories + " kcal");
+            System.out.println("Username: " + username);
+            System.out.println("Email: " + email);
+            System.out.println("Reminder Message: " + reminderMessage);
         });
 
         // Creating a scene
@@ -80,15 +76,7 @@ public class WellnessTracker extends Application {
         primaryStage.show();
     }
 
-    private void displayData(Exercise exercise, Workout workout, User user, Reminder reminder) {
-        System.out.println("Exercise: " + exercise.getName() + ", Duration: " + exercise.getDuration() + " mins, Calories Burned: " + exercise.getCalories());
-        System.out.println("Workout Date: " + workout.getDate() + ", Duration: " + workout.getDuration() + " mins, Calories Burned: " + workout.getCalories());
-        System.out.println("Username: " + user.getUsername() + ", Email: " + user.getEmail());
-        System.out.println("Reminder Time: " + reminder.getTime() + ", Message: " + reminder.getMessage());
-    }
-
     public static void main(String[] args) {
         launch(args);
     }
 }
-
