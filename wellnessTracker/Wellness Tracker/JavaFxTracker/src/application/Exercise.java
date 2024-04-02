0fd910c14ccc89042a720e7548c0e85425249066
AnthonyAl-khafaji <127707907+AnthonyAl-khafaji@users.noreply.github.com>
2024-04-02 15:45:52 -0400

    Update Exercise.java

--- wellnessTracker/Wellness Tracker/JavaFxTracker/src/application/Exercise.java
+++ wellnessTracker/Wellness Tracker/JavaFxTracker/src/application/Exercise.java
@@ -1,48 +1,18 @@
 package application;
 
 public class Exercise {
-    private int id;
     private String name;
-    private int duration; // in minutes
-    private int calories;
-
-    public Exercise(int id, String name, int duration, int calories) {
-        this.id = id;
+    
+    public Exercise(String name) {
         this.name = name;
-        this.duration = duration;
-        this.calories = calories;
     }
-
-    // Getters and setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    
+    // Getters and setters for name
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public int getCalories() {
-        return calories;
-    }
-
-    public void setCalories(int calories) {
-        this.calories = calories;
     }
 }
