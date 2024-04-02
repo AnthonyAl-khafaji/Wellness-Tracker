d70f024845c4c97624b81aee31475939b3b7daaf
AnthonyAl-khafaji <127707907+AnthonyAl-khafaji@users.noreply.github.com>
2024-04-02 15:47:36 -0400

    Update Workout.java

--- wellnessTracker/Wellness Tracker/JavaFxTracker/src/application/Workout.java
+++ wellnessTracker/Wellness Tracker/JavaFxTracker/src/application/Workout.java
@@ -3,47 +3,37 @@ package application;
 import java.util.Date;
 
 public class Workout {
-    private int id;
     private Date date;
-    private int duration; // in minutes
+    private int duration;
     private int calories;
-
-    public Workout(int id, Date date, int duration, int calories) {
-        this.id = id;
+    
+    public Workout(Date date, int duration, int calories) {
         this.date = date;
         this.duration = duration;
         this.calories = calories;
     }
-
-    // Getters and setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    
+    // Getters and setters for date, duration, and calories
     public Date getDate() {
         return date;
     }
-
+    
     public void setDate(Date date) {
         this.date = date;
     }
-
+    
     public int getDuration() {
         return duration;
     }
-
+    
     public void setDuration(int duration) {
         this.duration = duration;
     }
-
+    
     public int getCalories() {
         return calories;
     }
-
+    
     public void setCalories(int calories) {
         this.calories = calories;
     }
