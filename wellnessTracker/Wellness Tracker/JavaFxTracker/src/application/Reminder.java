3eed658c68b6dc239d095312c8aedd9a3fa08470
AnthonyAl-khafaji <127707907+AnthonyAl-khafaji@users.noreply.github.com>
2024-04-02 15:49:38 -0400

    Update Reminder.java

--- wellnessTracker/Wellness Tracker/JavaFxTracker/src/application/Reminder.java
+++ wellnessTracker/Wellness Tracker/JavaFxTracker/src/application/Reminder.java
@@ -3,37 +3,27 @@ package application;
 import java.util.Date;
 
 public class Reminder {
-    private int id;
-    private Date time;
+    private Date date;
     private String message;
-
-    public Reminder(int id, Date time, String message) {
-        this.id = id;
-        this.time = time;
+    
+    public Reminder(Date date, String message) {
+        this.date = date;
         this.message = message;
     }
-
-    // Getters and setters
-    public int getId() {
-        return id;
+    
+    // Getters and setters for date and message
+    public Date getDate() {
+        return date;
     }
-
-    public void setId(int id) {
-        this.id = id;
+    
+    public void setDate(Date date) {
+        this.date = date;
     }
-
-    public Date getTime() {
-        return time;
-    }
-
-    public void setTime(Date time) {
-        this.time = time;
-    }
-
+    
     public String getMessage() {
         return message;
     }
-
+    
     public void setMessage(String message) {
         this.message = message;
     }
