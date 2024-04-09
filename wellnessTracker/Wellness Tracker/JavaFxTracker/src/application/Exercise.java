9109ceb12168729a0c8e0294401b2d97f4d510e4
henryzachary <149612405+henryzachary@users.noreply.github.com>
2024-04-09 13:25:54 -0400

    Exercise.java

--- wellnessTracker/Wellness Tracker/JavaFxTracker/src/application/Exercise.java
+++ wellnessTracker/Wellness Tracker/JavaFxTracker/src/application/Exercise.java
@@ -1,17 +1,21 @@
 package application;
 
+// Exercise class represents an exercise entity
 public class Exercise {
+    // Private member variable to store the name of the exercise
     private String name;
     
+    // Constructor to initialize the exercise name
     public Exercise(String name) {
         this.name = name;
     }
     
-    // Getters and setters for name
+    // Getter method to retrieve the name of the exercise
     public String getName() {
         return name;
     }
     
+    // Setter method to set the name of the exercise
     public void setName(String name) {
         this.name = name;
     }
